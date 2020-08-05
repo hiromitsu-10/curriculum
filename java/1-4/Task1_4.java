7863941351924ffb55697b97358c923dd1515ffe
hiromitsu-10 <60024748+hiromitsu-10@users.noreply.github.com>
2020-08-05 21:42:54 +0900

    Update Task1_4.java

--- java/1-4/Task1_4.java
+++ java/1-4/Task1_4.java
@@ -8,8 +8,8 @@ public class Task1_4 {
 
     // 定数（条件）
     // ① 名前とパスワードの定数を定義してください。
-    String name = "alice";
-    String pass = "pass";
+    final String name = "名前";
+    final String pass = "パスワード";
     // 補足:
     // 定数は②〜⑤で使い回すために定義しておきます。
     // final修飾子を付与すると、変数（動的に変わる値）ではなく、定数（変更不可な定まった値）となります。
@@ -28,20 +28,20 @@ public class Task1_4 {
         String name = "alice";
         String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if (name == "alice" && pass == "pass") {
+        if (name.equals("alice") && pass.equals("pass")) {
             System.out.printIn(CONST_MSG_SUCCESS);
         }
-        ;
+
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        if (name == "alice" != pass == "pass") {
+        else if (name.equals("alice")) {
             System.out.printIn(CONST_MSG_ERROR_PASS);
         }
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        if (pass == "pass" != name == "alice") {
+        else if (pass.equals("pass")) {
             System.out.printIn(CONST_MSG_ERROR_NAME);
         }
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-        if (name == "alice" || pass == "pass") {
+        else {
             System.out.printIn(CONST_MSG_ERROR_INPUT);
         }
     }
